7d6e50e98026fdcb3cce4d03019033f574a2ea3c
imri <imri.paran@gmail.com>
2019-09-28 11:51:38 +0300

    added default

--- app/parse_expenses.py
+++ app/parse_expenses.py
@@ -4,13 +4,13 @@ log = logging.getLogger(__name__)
 
 
 def parse_file(bucket, key):
-    switcher = {"leumicard": id}
-    default = id
+    switcher = {"leumicard": lambda x: x}
+    default = lambda x: x
     res = switcher.get(key.split("/")[0], default)(f"s3://{bucket}/{key}")
     print(res)
 
 
 def handler(event, context):
     for r in event["Records"]:
-        log.info("r")
+        log.info(str(f))
         parse_file(r["s3"]["bucket"]["name"], r["s3"]["object"]["key"])
