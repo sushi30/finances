55ccac6dc891ad1647f20af8e712f9a8ea40cd7b
imri <imri.paran@mail.huji.ac.il>
2019-10-09 17:26:48 +0300

    more memory for server

--- app/__init__.py
+++ app/__init__.py
@@ -6,22 +6,3 @@ if os.getenv("ENV") == "LOCAL":
     from dotenv import load_dotenv
 
     load_dotenv()
-
-
-def create_app(test_config=None):
-    # create and configure the app
-    app = Flask(__name__)
-    app.config.from_mapping(
-        SECRET_KEY="dev", APP_NAME=os.environ.get("APP_NAME") or "Flask-Base"
-    )
-
-    @app.route("/test")
-    def test():
-        return "hi"
-
-    return app
-
-
-if __name__ == "__main__":
-    app = create_app()
-    app.run()
--- app/app.py
+++ app/app.py
@@ -1,3 +1,14 @@
-from app import create_app
+import os
 
-app = create_app()
+from flask import Flask
+
+
+app = Flask(__name__)
+app.config.from_mapping(
+    SECRET_KEY="dev", APP_NAME=os.environ.get("APP_NAME") or "Flask-Base"
+)
+
+
+@app.route("/test")
+def test():
+    return "hi"
