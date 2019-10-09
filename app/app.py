205157104b20c4418fb1875700f1ac3df7ecfa0f
imri <imri.paran@mail.huji.ac.il>
2019-10-09 17:28:00 +0300

    try flask lambda

--- app/app.py
+++ app/app.py
@@ -1,9 +1,7 @@
 import os
+from flask_lambda import FlaskLambda
 
-from flask import Flask
-
-
-app = Flask(__name__)
+app = FlaskLambda(__name__)
 app.config.from_mapping(
     SECRET_KEY="dev", APP_NAME=os.environ.get("APP_NAME") or "Flask-Base"
 )
