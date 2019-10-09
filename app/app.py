8ac70e2deccc8bfd8f5c517ee73f9a4139f926ea
imri <imri.paran@mail.huji.ac.il>
2019-10-09 21:23:39 +0300

    use flask lambda

--- app/app.py
+++ app/app.py
@@ -4,13 +4,13 @@ from flask_cors import CORS
 from flask_lambda import FlaskLambda
 
 app = FlaskLambda(__name__)
-CORS(app)
 app.config.from_mapping(
     SECRET_KEY="dev",
     APP_NAME=os.environ.get("APP_NAME") or "Flask-Base",
     FLASK_ENV=os.getenv("FLASK_ENV"),
     DEBUG=os.getenv("DEBUG"),
 )
+CORS(app)
 
 
 @app.route("/test")
