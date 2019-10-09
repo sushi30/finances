e9a16f4038562f215c732564f948cb434e429975
imri <imri.paran@mail.huji.ac.il>
2019-10-09 17:32:03 +0300

    added flask env

--- app/app.py
+++ app/app.py
@@ -3,7 +3,9 @@ from flask_lambda import FlaskLambda
 
 app = FlaskLambda(__name__)
 app.config.from_mapping(
-    SECRET_KEY="dev", APP_NAME=os.environ.get("APP_NAME") or "Flask-Base"
+    SECRET_KEY="dev",
+    APP_NAME=os.environ.get("APP_NAME") or "Flask-Base",
+    FLASK_ENV=os.getenv("FLASK_ENV"),
 )
 
 
