665b4630a6361bef0f434c6c5590133a61d58943
imri <imri.paran@gmail.com>
2019-10-20 17:33:53 +0300

    some temp authorization

--- auth/handlers.py
+++ auth/handlers.py
@@ -1,5 +1,16 @@
+from lambda_decorators import cors_headers, json_http_resp, load_json_body
+
 from auth.authorize import auth
 
 
 def authorize(event, context):
     return auth(event, context)
+
+
+@cors_headers
+@load_json_body
+def sign_in(event, context):
+    if event["body"]["username"] == "demo" and event["body"]["password"] == "1234":
+        return {"statusCode": 200, "token": "123454321"}
+    else:
+        return {"statusCode": 403}
