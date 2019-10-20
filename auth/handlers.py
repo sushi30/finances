9d320fa66c34b51b5a8d3c407b60614937ef99f3
imri <imri.paran@gmail.com>
2019-10-20 17:42:52 +0300

    dump json body

--- auth/handlers.py
+++ auth/handlers.py
@@ -1,4 +1,4 @@
-from lambda_decorators import cors_headers, json_http_resp, load_json_body
+from lambda_decorators import cors_headers, json_http_resp, load_json_body, dump_json_body
 
 from auth.authorize import auth
 
@@ -9,8 +9,9 @@ def authorize(event, context):
 
 @cors_headers
 @load_json_body
+@dump_json_body
 def login(event, context):
     if event["body"]["username"] == "demo" and event["body"]["password"] == "1234":
-        return {"statusCode": 200, "body": json.dumps({"token": "123454321"})}
+        return {"statusCode": 200, "body": {"token": "123454321"}}
     else:
         return {"statusCode": 403}
