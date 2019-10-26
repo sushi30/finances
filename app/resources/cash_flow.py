18db32bf42d98ff1d8bb3395c78f6c698b9205c0
imri <imri.paran@gmail.com>
2019-10-26 22:56:00 +0300

    added pageSize

--- app/resources/cash_flow.py
+++ app/resources/cash_flow.py
@@ -6,10 +6,11 @@ class CashFlow(Resource):
     def get(self):
         uuid = self.event["queryStringParameters"].get("id")
         page = self.event["queryStringParameters"].get("page", 0)
+        page_size = self.event["queryStringParameters"].get("size", 1)
         if uuid is None:
             records = CashFlowModel.to_records()
             return [{**o, "date": o["date"].isoformat()} for o in records][
-                20 * page : 20 * (page + 1)
+                page * page_size : page * (page_size + 1)
             ]
 
     def put(*args, **kwargs):
