af9b87aa762923d5bc174cfa236058f47a72a1b3
imri <imri.paran@gmail.com>
2019-10-26 22:46:23 +0300

    paging for cash flows

--- app/resources/cash_flow.py
+++ app/resources/cash_flow.py
@@ -5,9 +5,12 @@ from shared.resource import Resource
 class CashFlow(Resource):
     def get(self):
         uuid = self.event["queryStringParameters"].get("id")
+        page = self.event["queryStringParameters"].get("page", 0)
         if uuid is None:
             records = CashFlowModel.to_records()
-            return [{**o, "date": o["date"].isoformat()} for o in records][:30]
+            return [{**o, "date": o["date"].isoformat()} for o in records][
+                20 * page : 20 * (page + 1)
+            ]
 
     def put(*args, **kwargs):
         raise NotImplementedError()
