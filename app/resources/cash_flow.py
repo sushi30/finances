7bf74de13c80c06e893cd9358fea953f2b6255f6
imri <imri.paran@gmail.com>
2019-10-26 23:05:18 +0300

    get values as int

--- app/resources/cash_flow.py
+++ app/resources/cash_flow.py
@@ -5,8 +5,8 @@ from shared.resource import Resource
 class CashFlow(Resource):
     def get(self):
         uuid = self.event["queryStringParameters"].get("id")
-        page = self.event["queryStringParameters"].get("page", 0)
-        page_size = self.event["queryStringParameters"].get("size", 1)
+        page = int(self.event["queryStringParameters"].get("page", 0))
+        page_size = int(self.event["queryStringParameters"].get("size", 1))
         if uuid is None:
             records = CashFlowModel.to_records()
             return [{**o, "date": o["date"].isoformat()} for o in records][
