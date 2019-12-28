886d32d8a6237f7deac5388282c21815723d62a3
imri <imri.paran@mail.huji.ac.il>
2019-12-28 17:12:42 +0200

    parameterized storage

--- scripts/create_tables.py
+++ scripts/create_tables.py
@@ -1,14 +1,16 @@
+import sys
+
 from sqlalchemy import create_engine
 from app.models.cash_flow import CashFlow
 from app.models.cash_flow_mapping import SpecificCashFlowMapping, GeneralCashFlowMapping
 
 
-def main():
-    engine = create_engine("mysql://root:1234@localhost/finance")
-    # CashFlow.metadata.create_all(engine)
+def main(storage):
+    engine = create_engine(storage)
+    CashFlow.metadata.create_all(engine)
     SpecificCashFlowMapping.metadata.create_all(engine)
     GeneralCashFlowMapping.metadata.create_all(engine)
 
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1])
